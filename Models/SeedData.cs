7bc8e970eb050bdf27dbc6b7b800da31f9fc9617
Georgina JEZABEL <georginaj@Cogelis.local>
2025-03-12 12:43:41 +0100

    BD Seeding

---
+++ Models/SeedData.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+
+namespace RazorPagesMovie.Models;
+
+public static class SeedData //expresa la lógica para  "sembrar la base de datos"
+{
+    public static void Initialize(IServiceProvider serviceProvider) //método estático que utiliza el parámetro IServiceProvider
+                                                                    // usado para resolver las dependencias en el contexto de la BD
+    {
+        using (var context = new RazorPagesMovieContext(        //crea un contexto de base de datos para la conexión a la BD y conectarse la tabla Movie
+            serviceProvider.GetRequiredService<
+                DbContextOptions<RazorPagesMovieContext>>()))       //Recupera el contexto "RazorPagesMovieContext"
+        {
+            if (context == null || context.Movie == null)   //revisa la disponibilidad de la BD y lanza el mensaje en caso de indisponibilidad
+            {
+                throw new ArgumentNullException("Null RazorPagesMovieContext");
+            }
+
+            // Look for any movies.
+            if (context.Movie.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Movie.AddRange(     //Agrega unr anto de objetos  al objeto 'Movie' de la BD
+                new Movie
+                {
+                    Title = "When Harry Met Sally",
+                    ReleaseDate = DateTime.Parse("1989-2-12"),
+                    Genre = "Romantic Comedy",
+                    Price = 7.99M
+                },
+
+                new Movie
+                {
+                    Title = "Ghostbusters ",
+                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Genre = "Comedy",
+                    Price = 8.99M
+                },
+
+                new Movie
+                {
+                    Title = "Ghostbusters 2",
+                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    Genre = "Comedy",
+                    Price = 9.99M
+                },
+
+                new Movie
+                {
+                    Title = "Rio Bravo",
+                    ReleaseDate = DateTime.Parse("1959-4-15"),
+                    Genre = "Western",
+                    Price = 3.99M
+                }
+            );
+            context.SaveChanges(); //Guarda todos los datos en la BD
+        }
+    }
+}
--- obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+830890eb8d4a83c3cea6e923d58523cb9e84b4f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2980a50b4288c602aa1d76f040e20fb588c64f23")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
